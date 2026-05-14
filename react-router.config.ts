9fec44eb7692446af51546026fe15fab54e4cdad
JonathanSum(Alice) <777Jonathansum@gmail.com>
2026-05-14 09:44:08 -0700

    Configure deployment for AliceSum/Profile on GitHub Pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,9 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,  //Turn off this github page deployment
+  basename: "/Profile/",
+
+  async prerender() {
+    return ["/", "/projects"];
+  },
 } satisfies Config;
